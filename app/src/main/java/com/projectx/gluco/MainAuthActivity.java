e01a0123e346bc5b29851256801f71a91ffd8977
Patrick Prakash <patrickprakash8@gmail.com>
2018-02-01 21:04:55 +0530

    Created Main auth file

---
+++ app/src/main/java/com/projectx/gluco/MainAuthActivity.java
@@ -0,0 +1,13 @@
+package com.projectx.gluco;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainAuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_auth);
+    }
+}
