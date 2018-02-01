fe8f1a07e7f86540b67c506c636fb879cb82b7aa
Patrick Prakash <patrickprakash8@gmail.com>
2018-02-01 20:41:41 +0530

    Changed File names

--- app/src/main/java/com/projectx/gluco/RegisterMailActivity.java
+++ app/src/main/java/com/projectx/gluco/RegisterMailActivity.java
@@ -1,34 +1,34 @@
 package com.projectx.gluco;
 
+import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterMailActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
     private EditText uEmail;
     private EditText uPass;
     private Button uBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
+        setContentView(R.layout.activity_email_signup);
 
 
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
 
-                if(firebaseAuth.getCurrentUser() != null)
-                {
+                if (firebaseAuth.getCurrentUser() != null) {
 
 
                 }
