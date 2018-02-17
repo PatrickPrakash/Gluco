29165865a0d5eed0ec8dcea9a035b6638cf2b388
Patrick Prakash <patrickprakash8@gmail.com>
2018-02-17 23:56:31 +0530

    Implemented Signout

--- app/src/main/java/com/projectx/gluco/Authentication/MainAuthActivity.java
+++ app/src/main/java/com/projectx/gluco/Authentication/MainAuthActivity.java
@@ -1,8 +1,9 @@
 package com.projectx.gluco.Authentication;
+
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -25,13 +26,13 @@ import com.projectx.gluco.R;
 
 public class MainAuthActivity extends AppCompatActivity {
 
-    private Button g_signin;
-    private Button e_signin;
-    GoogleSignInClient mGoogleSignInClient;
-    FirebaseAuth mAuth;
     private final static int RC_SIGN_IN = 1;
     private static final String TAG = "GoogleActivity";
+    GoogleSignInClient mGoogleSignInClient;
+    FirebaseAuth mAuth;
     FirebaseAuth.AuthStateListener mAuthListener;
+    private Button g_signin;
+    private Button e_signin;
 
     @Override
     protected void onResume() {
@@ -90,6 +91,9 @@ public class MainAuthActivity extends AppCompatActivity {
         startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
+    public void signOut() {
+        FirebaseAuth.getInstance().signOut();
+    }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
--- app/src/main/java/com/projectx/gluco/Fragments/AboutFragment.java
+++ app/src/main/java/com/projectx/gluco/Fragments/AboutFragment.java
@@ -1,30 +1,79 @@
 package com.projectx.gluco.Fragments;
 
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.projectx.gluco.Authentication.MainAuthActivity;
 import com.projectx.gluco.R;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class AboutFragment extends Fragment {
 
+public class AboutFragment extends Fragment implements View.OnClickListener {
+    Context context;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthstateListener;
 
     public AboutFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        this.context = context;
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDestroy() {
+        //   MainAuthActivity auth = new MainAuthActivity();
+        // auth.signOut();
+        super.onDestroy();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        View rootview = inflater.inflate(R.layout.fragment_about, container, false);
+
+        //Get a reference to the layout
+        Button msignout = rootview.findViewById(R.id.msignout);
+        msignout.setOnClickListener(this);
+        return rootview;
+
     }
 
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.msignout:
+                //Toast.makeText(Context, "Button Clicked", Toast.LENGTH_SHORT).show();
+                MainAuthActivity auth = new MainAuthActivity();
+                auth.signOut();
+                Intent i = new Intent(context, MainAuthActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(i);
+                break;
+            default:
+                break;
+        }
+    }
 }
