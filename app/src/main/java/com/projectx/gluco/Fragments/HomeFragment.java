31aa2b17e85880688ad07c79c49a87b5f702fb8d
Patrick Prakash <patrickprakash8@gmail.com>
2018-02-18 17:09:29 +0530

    Redesigned the home fragment

--- app/src/main/java/com/projectx/gluco/Fragments/HomeFragment.java
+++ app/src/main/java/com/projectx/gluco/Fragments/HomeFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.projectx.gluco.R;
 
@@ -24,7 +26,20 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        View rootview = inflater.inflate(R.layout.fragment_home, container, false);
+        //Spinner date
+        Spinner date_spinner = rootview.findViewById(R.id.date_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getActivity(), R.array.day_spinner, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        date_spinner.setAdapter(adapter);
+
+        //Spinner medical
+        Spinner med_spinner = rootview.findViewById(R.id.med_spinner);
+        ArrayAdapter<CharSequence> med_adapter = ArrayAdapter.createFromResource(this.getActivity(), R.array.med_spinner, android.R.layout.simple_spinner_item);
+        med_adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        med_spinner.setAdapter(med_adapter);
+        return rootview;
     }
 
 }
