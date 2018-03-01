ffad070e43c095afd234a98f7850d3b022c5aa6b
Patrick Prakash <patrickprakash8@gmail.com>
2018-03-01 19:17:24 +0530

    Added recyclerview

--- app/src/main/java/com/projectx/gluco/Fragments/LogFragment.java
+++ app/src/main/java/com/projectx/gluco/Fragments/LogFragment.java
@@ -1,81 +1,112 @@
 package com.projectx.gluco.Fragments;
 
-
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.projectx.gluco.DataModels.Modeli;
 import com.projectx.gluco.R;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class LogFragment extends Fragment {
     RecyclerView recyclerView;
+    DatabaseReference databaseReference;
+    FirebaseAuth firebaseAuth;
 
     public LogFragment() {
-        // Required empty public constructor
+
     }
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //Inflate the Android Log fragment
+        View rootView = inflater.inflate(R.layout.fragment_log, container, false);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser(); //To get uid
+        String uid = user.getUid();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        databaseReference.child("User_Readings").child(uid).child("Blood_Glucose");
+        databaseReference.keepSynced(true);
+        recyclerView = rootView.findViewById(R.id.fragment_history_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setHasFixedSize(true);
+        return rootView;
+    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        // TODO Complete Recycler View and Card view Integrated with Firebase.
-        View rootview = inflater.inflate(R.layout.fragment_log, container, false);
+    public void onStart() {
+        super.onStart();
+        FirebaseRecyclerAdapter<Modeli, BloodViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Modeli, BloodViewHolder>(Modeli.class, R.layout.fragment_history_item, BloodViewHolder.class, databaseReference) {
+            public static final String TAG = "Fire";
 
-        recyclerView = rootview.findViewById(R.id.fragment_history_recycler_view);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        recyclerView.setAdapter(new RecyclerViewAdapter());
-        return rootview;
+            @Override
+            protected void populateViewHolder(BloodViewHolder viewHolder, Modeli model, int position) {
+                Log.v(TAG, "Value" + model.getDate());
+                viewHolder.setGConcentration(model.getConcentration());
+                viewHolder.setGDate(model.getDate());
+                viewHolder.setGNotes(model.getNotes());
+                viewHolder.setGTime(model.getTime());
+            }
+        };
+        recyclerView.setAdapter(firebaseRecyclerAdapter);
+
     }
 
-    private class RecyclerViewHolder extends RecyclerView.ViewHolder
+    public static class BloodViewHolder extends RecyclerView.ViewHolder {
 
-    {
-        private CardView item_history;
-        private TextView item_history_time, item_history_date, item_history_reading, item_history_notes;
+        CardView mCardview;
+        TextView item_history_time, item_history_date, item_history_notes, item_history_reading;
 
-        public RecyclerViewHolder(View itemView) {
+        public BloodViewHolder(View itemView) {
             super(itemView);
         }
 
-        public RecyclerViewHolder(LayoutInflater inflater, ViewGroup container) {
+        public BloodViewHolder(LayoutInflater inflater, ViewGroup container) {
             super(inflater.inflate(R.layout.fragment_history_item, container, false));
-            item_history = itemView.findViewById(R.id.item_history);
-            item_history_date = itemView.findViewById(R.id.item_history_date);
-            item_history_time = itemView.findViewById(R.id.item_history_time);
+            mCardview = itemView.findViewById(R.id.item_history);
+
+        }
+
+        public void setGConcentration(String concentration) {
             item_history_reading = itemView.findViewById(R.id.item_history_reading);
+            item_history_reading.setText(concentration);
+        }
+
+        public void setGDate(String date) {
+            item_history_date = itemView.findViewById(R.id.item_history_date);
+            item_history_date.setText(date);
+        }
+
+        public void setGTime(String time) {
+            item_history_time = itemView.findViewById(R.id.item_history_time);
+            item_history_time.setText(time);
+
+        }
+
+        public void setGNotes(String notes) {
             item_history_notes = itemView.findViewById(R.id.item_history_notes);
+            item_history_notes.setText(notes);
 
         }
     }
 
-    private class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewHolder> {
 
-
-        @Override
-        public RecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
-            return new RecyclerViewHolder(layoutInflater, parent);
-        }
-
-        @Override
-        public void onBindViewHolder(RecyclerViewHolder holder, int position) {
-
-        }
-
-
-        @Override
-        public int getItemCount() {
-            return 10;
-        }
-    }
 }
+
